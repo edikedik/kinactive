c6f84888f739e27e633c24e549894fc74123f3c3
edikedik <edikedikedikedik@gmail.com>
2023-05-19 15:57:07 +0200

    improve db protocol

--- kinactive/db.py
+++ kinactive/db.py
@@ -8,6 +8,7 @@ from collections import abc
 from io import StringIO
 from itertools import chain
 from pathlib import Path
+from random import sample
 
 import lXtractor.core.chain.tree as chain_tree
 import lXtractor.core.config as lx_cfg
@@ -20,13 +21,14 @@ from lXtractor.core.chain import (
     ChainStructure,
 )
 from lXtractor.ext import PDB, PyHMMer, SIFTS, fetch_uniprot, filter_by_method
-from lXtractor.util import get_files, read_fasta, write_fasta
+from lXtractor.util import get_files, read_fasta, write_fasta, apply
 from more_itertools import ilen, take
 from toolz import curry, groupby, itemmap, keyfilter, keymap
 from tqdm.auto import tqdm
 
 from kinactive.config import DBConfig
 
+T = t.TypeVar("T")
 CT_: t.TypeAlias = Chain | ChainSequence | ChainStructure
 LOGGER = logging.getLogger(__name__)
 
@@ -54,8 +56,8 @@ def _is_sequence_of_chain_seqs(
 
 
 def _stage_chain_init(
-    seq: ChainSequence, pdb_chains: abc.Iterable[str], pdb_dir: Path, fmt: str
-) -> tuple[ChainSequence, list[tuple[Path, list[str]]]]:
+    seq: T, pdb_chains: abc.Iterable[str], pdb_dir: Path, fmt: str
+) -> tuple[T, list[tuple[Path, list[str]]]]:
     id2chains = groupby(op.itemgetter(0), map(lambda x: x.split(":"), pdb_chains))
     path2chains = itemmap(
         lambda x: (pdb_dir / f"{x[0]}.{fmt}", list(map(op.itemgetter(1), x[1]))),
@@ -96,9 +98,17 @@ class DB:
         return self._chains
 
     def _load_sifts(self, overwrite: bool = False) -> SIFTS:
-        if self._sifts is None or overwrite:
-            self._sifts = SIFTS(load_segments=False, load_id_mapping=True)
-        return self._sifts
+        if self._sifts is not None and not overwrite:
+            sifts = self._sifts
+        else:
+            sifts = SIFTS(load_segments=False, load_id_mapping=True)
+
+        if sifts.id_mapping is None:
+            LOGGER.info("Initializing SIFTS for the first time.")
+            sifts.fetch()
+            sifts.parse()
+
+        return sifts
 
     def _load_pdb(self, overwrite: bool = False) -> PDB:
         if self._pdb is None or overwrite:
@@ -128,14 +138,14 @@ class DB:
             id_ = header.split("|")[1]
             write_fasta([(header, seq)], self.cfg.seq_dir / f"{id_}.fasta")
 
-    def _read_seqs(self, ids: abc.Iterable[str]) -> ChainList[ChainSequence]:
+    def _read_seqs(self, ids: abc.Iterable[str]) -> ChainList[Chain]:
         files = keymap(lambda x: x.removesuffix(".fasta"), get_files(self.cfg.seq_dir))
         matching = set(files) & set(ids)
         paths = keyfilter(lambda x: x in matching, files).values()
         init = ChainInitializer(verbose=self.cfg.verbose)
         chains = list(init.from_iterable(paths))
         assert _is_sequence_of_chain_seqs(chains), "correct types are returned"
-        return ChainList(chains)
+        return ChainList(map(Chain, chains))
 
     def _get_sifts_xray(self) -> list[str]:
         sifts = self._load_sifts()
@@ -144,10 +154,19 @@ class DB:
 
     def build(
         self,
+        uniprot_ids: abc.Collection[str] | None = None,
+        pdb_chain_ids: abc.Collection[str] | None = None,
+        n_domains: int = 0,
     ) -> ChainList[Chain]:
         """
         Build a new lXt-PK data collection.
 
+        :param uniprot_ids: An optional list of UniProt IDs to restrict
+            the db to.
+        :param pdb_chain_ids: An optional collection of PDB chains to restrict
+            the db to. Format: "{PDB_ID}:{ChainID}".
+        :param n_domains: Use n random sequence domains. It is helpful for
+            testing the pipeline.
         :return: A :class:`ChainList` of :class:`Chain` objects having at least
             one child PK domain with at least one PK domain structure passing
             the filtering thresholds.
@@ -185,8 +204,17 @@ class DB:
         pdb = self._load_pdb()
 
         # Fetch SIFTS UniProt seqs
-        fetch_ids = _get_remaining(sifts.uniprot_ids, self.cfg.seq_dir)
-        LOGGER.info(f"{len(fetch_ids)} remaining sequences to fetch")
+        if uniprot_ids:
+            ids = list(filter(lambda x: x.isin(uniprot_ids), sifts.uniprot_ids))
+            LOGGER.info(
+                f"Filtered to {len(ids)} out of {len(uniprot_ids)} initially provided "
+                f"UniProt IDs."
+            )
+        else:
+            ids = sifts.uniprot_ids
+
+        fetch_ids = _get_remaining(ids, self.cfg.seq_dir)
+        LOGGER.info(f"{len(fetch_ids)} remaining sequences to fetch.")
         self._fetch_seqs(fetch_ids)
 
         # Read
@@ -195,12 +223,12 @@ class DB:
 
         # Filter sequences by size
         min_size, max_size = self.cfg.min_seq_size, self.cfg.max_seq_size
-        seqs = seqs.filter(lambda s: min_size <= len(s) <= max_size)
+        seqs = seqs.filter(lambda s: min_size <= len(s.seq) <= max_size)
         LOGGER.info(f"Filtered to {len(seqs)} seqs in [{min_size}, {max_size}]")
 
         # Annotate PK domains and filter seqs to the annotated ones
         pk_hmm = self._load_pk_hmm()
-        ann: abc.Iterable[ChainSequence] = pk_hmm.annotate(
+        ann: abc.Iterable[Chain] = pk_hmm.annotate(
             seqs,
             new_map_name=self.cfg.pk_map_name,
             min_score=self.cfg.pk_min_score,
@@ -212,22 +240,34 @@ class DB:
             ann = tqdm(ann, desc="Annotating sequence domains")
         annotated_num = sum(1 for _ in ann)
         seqs = seqs.filter(lambda x: len(x.children) > 0)
-        LOGGER.info(f"Found {annotated_num} PK domains within {len(seqs)} seqs")
+        LOGGER.info(f"Found {annotated_num} PK domains within {len(seqs)} seqs.")
 
-        # Get IDs UniProt IDs and corresponding PDB Chains
+        if n_domains:
+            seqs = ChainList(sample(seqs, n_domains))
+            LOGGER.info(f"Sampled to {len(seqs)} random initial domains.")
+
+        # Get UniProt IDs and corresponding PDB Chains
         uni_ids = [x.id.split("|")[1] for x in seqs]
         pdb_chains = [x for x in map(sifts.map_id, uni_ids) if x is not None]
 
+        # Filter PDB IDs to provided list
+        if pdb_chain_ids:
+            pdb_chains = list(filter(lambda x: x in pdb_chain_ids, pdb_chains))
+            LOGGER.info(
+                f"Filtered to {len(pdb_chains)} out of {len(pdb_chain_ids)} "
+                f"initially provided PDB chain IDs."
+            )
+
         # Filter PDB IDs to X-ray structures
         pdb_ids = {x.split(":")[0] for x in chain.from_iterable(pdb_chains)}
-        LOGGER.info(f"Fetching info for {len(pdb_ids)} PDB IDs")
+        LOGGER.info(f"Fetching info for {len(pdb_ids)} PDB IDs.")
         xray_pdb_ids = set(
             filter_by_method(
                 pdb_ids, pdb=pdb, dir_=self.cfg.pdb_dir_info, method="X-ray"
             )
         )
         LOGGER.info(
-            f"Filtered to {len(xray_pdb_ids)} X-ray PDB IDs out of {len(pdb_ids)}"
+            f"Filtered to {len(xray_pdb_ids)} X-ray PDB IDs out of {len(pdb_ids)}."
         )
         pdb_chains = [
             [c for c in cs if c.split(":")[0] in xray_pdb_ids] for cs in pdb_chains
@@ -250,33 +290,10 @@ class DB:
                 val_callbacks=[_rm_solvent, curry(_filter_by_size)(cfg=self.cfg)],
                 num_proc_read_str=self.cfg.init_cpus,
                 num_proc_map_numbering=self.cfg.init_map_numbering_cpus,
+                add_to_children=True
             )
         ).filter(lambda c: len(c.structures) > 0)
-        LOGGER.info(f"Initialized {len(chains)} `Chain` objects")
-
-        _chains = (
-            tqdm(chains, desc="Subsetting `Chain`s by domain boundaries")
-            if self.cfg.verbose
-            else chains
-        )
-        for c in _chains:
-            for seq_child in c.seq.children:
-                try:
-                    child = c.spawn_child(
-                        seq_child.start,
-                        seq_child.end,
-                        seq_child.name,
-                        str_map_from=lx_cfg.SeqNames.map_canonical,
-                        tolerate_failure=True,
-                    )
-                    pk_name = self.cfg.pk_map_name
-                    child.seq.add_seq(pk_name, seq_child[pk_name])
-                    child.seq.meta = seq_child.meta
-                except Exception as e:
-                    raise RuntimeError(
-                        f"Unexpected exception when trying to init child {seq_child} "
-                        f"for Chain {c}"
-                    ) from e
+        LOGGER.info(f"Initialized {len(chains)} `Chain` objects.")
 
         num_init = ilen(chains.collapse_children().iter_structures())
         chains = chains.apply(
@@ -296,14 +313,14 @@ class DB:
         num_curr = len(chains.collapse_children())
         LOGGER.info(
             f"Filtered to {num_curr} out of {num_init} domains with "
-            "at least one valid structures"
+            "at least one valid structures."
         )
 
         num_init = len(chains)
         chains = chains.filter(lambda c: len(c.children) > 0)
         LOGGER.info(
             f"Filtered to {len(chains)} chains out of {num_init} "
-            "with at least one extracted domains"
+            "with at least one extracted domains."
         )
 
         for c in chains.collapse_children():
@@ -314,7 +331,10 @@ class DB:
         return chains
 
     def save(
-        self, dest: Path, chains: abc.Iterable[Chain] | None = None
+        self,
+        dest: Path | None = None,
+        chains: abc.Iterable[Chain] | None = None,
+        overwrite: bool = False,
     ) -> abc.Iterator[Path]:
         """
         Save DB sequence to file system.
@@ -322,15 +342,18 @@ class DB:
         :param dest: Destination path to write seqs into.
         :param chains: Manual chains input to save. If ``None``, will use
             :attr:`chains`.
+        :param overwrite: Overwrite existing data in ``dest``.
         :return: An iterator over paths of successfully saved chains. Consume
             to trigger saving.
         """
         chains = chains or self.chains
+        dest = dest or self.cfg.target_dir
         if dest.exists():
             assert dest.is_dir(), "Path to directory"
-            files = get_files(dest)
-            assert len(files) == 0, "Existing dir is empty"
-        dest.mkdir(exist_ok=True, parents=True)
+            if not overwrite:
+                files = get_files(dest)
+                assert len(files) == 0, "Existing dir is not empty"
+        # dest.mkdir(exist_ok=True, parents=True)
         io = ChainIO(
             num_proc=self.cfg.io_cpus, verbose=self.cfg.verbose, tolerate_failures=False
         )
