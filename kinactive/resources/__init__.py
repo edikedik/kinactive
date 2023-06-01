517f3031fa9b6f7b0da76e9b454775a79e4aa9db
edikedik <edikedikedikedik@gmail.com>
2023-06-01 14:08:58 +0300

    add module desc

---
+++ kinactive/resources/__init__.py
@@ -0,0 +1,8 @@
+"""
+This module contains persistent resources downloaded alongside the Python code.
+
+It contains the PK domain HMM profile and the models: ``DFG_classifier`` and
+``kinactive_classifier``. The functions :func:`kinactive.io.load_dfg` and
+:func:`kinactive.io.load_kinactive` are used to load these into the Python
+interpreter.
+"""
