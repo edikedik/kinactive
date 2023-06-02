4801eb5bf00d54904380c530f267390fbb2c0c9a
edikedik <edikedikedikedik@gmail.com>
2023-06-02 14:08:08 +0300

    add imports

--- kinactive/__init__.py
+++ kinactive/__init__.py
@@ -1,4 +1,5 @@
 from .config import DBConfig, MatrixConfig, DefaultMatrixConfig
 from .db import DB
-from .features import DefaultFeatures
+from .features import DefaultFeatures, calculate
 from .distances import DistanceMatrix
+from .io import load_dfg, load_kinactive
